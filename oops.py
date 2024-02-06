90cb15d118a6a4d9bc4b4a680834c58bb7c23f9f
rupa <ruparupa8887@gmail.com>
2024-02-06 11:48:04 +0530

    added oops.py

---
+++ oops.py
@@ -0,0 +1,23 @@
+'''class employee:
+  id=10
+  name="devansh"
+def display(self):
+    print(self.id,self.name)
+class Student:
+    def __init__(self,age,name):
+       self.age=age
+       self.name=Name
+    def display(self):
+        print(self.age,self.name)'''
+
+class Student:
+    def __init__(self, age, name):
+        self.age = age
+        self.name = name
+    
+    def display(self):
+        print(self.age, self.name)
+    #student1=student(age=10,name=rupa)
+    #student1.display()
+        age=22
+        name=rupa
